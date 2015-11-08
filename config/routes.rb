e03a5297b3e08da3fa51055137a8496ee1b28fa9
BBustamante <bzbustamante@gmail.com>
2015-11-07 20:04:40 -0500

    blog ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-resources :articles
+resources :articles do
+  resources :comments
+end
 root 'welcome#index'
 
 # article GET  /articles/:id(.:format)  articles#show
